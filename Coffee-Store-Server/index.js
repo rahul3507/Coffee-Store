3924389a7a93a302c133469adea2ee4bca9c24e8
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-01 10:28:29 +0600

    fix: adjust MongoDB connection logic to ensure client remains open for debugging

--- Coffee-Store-Server/index.js
+++ Coffee-Store-Server/index.js
@@ -28,6 +28,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -35,7 +36,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
