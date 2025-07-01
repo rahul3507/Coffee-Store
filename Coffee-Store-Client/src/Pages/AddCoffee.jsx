a276ce384017a3529a89f4afb3df53c67340bc74
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-01 11:56:34 +0600

    feat: add category and details fields to AddCoffee form

--- Coffee-Store-Client/src/Pages/AddCoffee.jsx
+++ Coffee-Store-Client/src/Pages/AddCoffee.jsx
@@ -76,6 +76,39 @@ const AddCoffee = () => {
             </div>
           </div>
 
+          {/* form category and details row */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="category"
+              >
+                Category
+              </label>
+              <Input
+                type="text"
+                id="category"
+                placeholder="Category"
+                className="shadow  appearance-none bg-white border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="details"
+              >
+                Details
+              </label>
+              <Input
+                type="text"
+                id="details"
+                placeholder="Details"
+                className="shadow bg-white appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+          </div>
+
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
