3b1336e549095722ab649354e31f09a9a31220b8
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-01 11:47:55 +0600

    feat: add Input component and update AddCoffee page with form for coffee details

--- Coffee-Store-Client/src/Pages/AddCoffee.jsx
+++ Coffee-Store-Client/src/Pages/AddCoffee.jsx
@@ -1,9 +1,58 @@
 /** @format */
 
 import React from "react";
+import { Input } from "../components/ui/input";
 
 const AddCoffee = () => {
-  return <div>AddCoffee</div>;
+  return (
+    <div className="justify-center items-center p-4 flex min-h-screen ">
+      <div className="bg-[#F4F3F0]  min-w-full md:min-w-xl lg:min-w-2xl xl:min-w-3xl  m-auto   rounded-lg p-6">
+        <h1 className="text-3xl text-center font-bold">Add Coffee</h1>
+
+        <form className="mt-4">
+          {/* form name and quantity row */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Coffee Name
+              </label>
+              <Input
+                type="text"
+                id="name"
+                placeholder="Enter coffee name"
+                className="shadow  appearance-none bg-white border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="origin"
+              >
+                Available Quantity
+              </label>
+              <Input
+                type="text"
+                id="quantity"
+                placeholder="Available quantity"
+                className="shadow bg-white appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Add Coffee
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddCoffee;
