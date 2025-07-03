41a8512bf9030c7aa56f7ed18dc656263ab89073
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-03 08:37:30 +0600

    feat: restructure App component layout for better organization and readability

--- Coffee-Store-Client/src/App.jsx
+++ Coffee-Store-Client/src/App.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import CoffeeCard from "./components/common/CoffeeCard";
 import { useState } from "react";
 
@@ -14,9 +14,17 @@ function App() {
   return (
     <>
       <div className="m-1 md:m-3 lg:m-5">
-        <h1 className="text-3xl text-orange-300 text-center font-bold mb-7">
-          Total Coffees: {coffees.length}
-        </h1>
+        <div className="flex flex-col w-full items-center mb-5">
+          <h1 className="text-3xl text-orange-300 text-center font-bold mb-7">
+            Total Coffees: {coffees.length}
+          </h1>
+          <Link
+            to="/add-coffee"
+            className=" inline-block bg-amber-300 hover:bg-amber-400 text-black font-bold py-2 px-4 rounded"
+          >
+            Add Coffee
+          </Link>
+        </div>
         <div className="flex flex-wrap  gap-3 justify-center items-center">
           {coffees.map((coffee) => (
             <CoffeeCard
