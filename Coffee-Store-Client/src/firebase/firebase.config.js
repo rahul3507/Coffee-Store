4346331ed0f7e88df9f0856f0aed1571076a0f54
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-07-03 09:35:10 +0600

    feat: add Firebase configuration and initialization

---
+++ Coffee-Store-Client/src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+/** @format */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAbwRf-MxahpT4WJfV_J0edLt2Ts1d-0so",
+  authDomain: "coffee-store-8e261.firebaseapp.com",
+  projectId: "coffee-store-8e261",
+  storageBucket: "coffee-store-8e261.firebasestorage.app",
+  messagingSenderId: "986105380989",
+  appId: "1:986105380989:web:ecb648b810aa7f4352f114",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
